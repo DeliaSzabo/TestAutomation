6fd8414d03f7895ffcdc29ed8ed253522daf515c
DESKTOP-GK38374\delia <delia.a.szabo@gmail.com>
2022-04-16 13:34:11 +0300

    ignore .class files

--- src/test/java/AutomationWeek9/LibraryTest.java
+++ src/test/java/AutomationWeek9/LibraryTest.java
@@ -11,6 +11,6 @@ public class LibraryTest {
 	@Test
 	public void testSomeLibraryMethod() {
 		Library classUnderTest = new Library();
-		assertTrue("someLibraryMethod should return 'false'", classUnderTest.someLibraryMethod());
+		assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
 	}
 }
