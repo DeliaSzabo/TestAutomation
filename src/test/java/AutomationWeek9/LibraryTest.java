a40076c888c1b89a4b62204fbd31bf3a59edbfc0
DESKTOP-GK38374\delia <delia.a.szabo@gmail.com>
2022-04-30 13:43:57 +0300

    Added new class.

--- src/test/java/AutomationWeek9/LibraryTest.java
+++ src/test/java/AutomationWeek9/LibraryTest.java
@@ -3,14 +3,46 @@
  */
 package AutomationWeek9;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LibraryTest {
+	// @Test
+	// public void testSomeLibraryMethod() {
+	// Library classUnderTest = new Library();
+	// assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+	// }
+
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before all test in class");
+	}
+
+	@Before
+	public void before() {
+		System.out.println("Before each test method");
+	}
+
+	@After
+	public void after() {
+		System.out.println("After each test in test method");
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After all test in class");
+	}
+
 	@Test
-	public void testSomeLibraryMethod() {
-		Library classUnderTest = new Library();
-		assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+	public void test01() {
+		System.out.println("Test 1");
+	}
+
+	@Test
+	public void test02() {
+		System.out.println("Test 2");
 	}
 }
